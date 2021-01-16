e6be74aac7f47326074e2cb6085f90f62c9bc4b0
Wu <wuzhouxiang2012@gmail.com>
2021-01-16 11:37:51 -0600

    update

--- RL_network_operator/project/distribution.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-
-
-class Distribution:
-    def __init__(self, id:int, vals:List[float], probs:List[float])->None:
-        self.id = id
-        self.isStatic = len(vals)==1
-        self.vals = vals
-        self.probs = probs
